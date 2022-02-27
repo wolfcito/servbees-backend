4c87dfeb1b93cbf6c417f2eb27d1975f572bca9b
Luis Fernando Ushiña <guffenix@gmail.com>
2022-02-26 23:44:44 -0500

    usuario microservice

---
+++ servbees-usuario/dominio/src/test/java/com/ceiba/usuario/entidad/UsuarioTest.java
@@ -0,0 +1,80 @@
+package com.ceiba.usuario.entidad;
+
+import com.ceiba.BasePrueba;
+import com.ceiba.dominio.excepcion.ExcepcionLongitudValor;
+import com.ceiba.dominio.excepcion.ExcepcionValorObligatorio;
+import com.ceiba.usuario.modelo.entidad.Usuario;
+import com.ceiba.usuario.servicio.testdatabuilder.UsuarioTestDataBuilder;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UsuarioTest {
+
+    @Test
+    @DisplayName("Deberia crear correctamente el usuario")
+    void deberiaCrearCorrectamenteElUsusuario() {
+        // arrange
+        LocalDateTime fechaCreacion = LocalDateTime.now();
+        //act
+        Usuario usuario = new UsuarioTestDataBuilder().conFechaCreacion(fechaCreacion).conId(1L).build();
+        //assert
+        assertEquals(1, usuario.getId());
+        assertEquals("1234", usuario.getNombre());
+        assertEquals("1234", usuario.getClave());
+        assertEquals(fechaCreacion, usuario.getFechaCreacion());
+    }
+
+    @Test
+    void deberiaFallarSinNombreDeUsuario() {
+
+        //Arrange
+        UsuarioTestDataBuilder usuarioTestDataBuilder = new UsuarioTestDataBuilder().conNombre(null).conId(1L);
+        //act-assert
+        BasePrueba.assertThrows(() -> {
+                    usuarioTestDataBuilder.build();
+                },
+                ExcepcionValorObligatorio.class, "Se debe ingresar el nombre de usuario");
+    }
+
+    @Test
+    void deberiaFallarSinClave() {
+
+        //Arrange
+        UsuarioTestDataBuilder usuarioTestDataBuilder = new UsuarioTestDataBuilder().conClave(null).conId(1L);
+        //act-assert
+        BasePrueba.assertThrows(() -> {
+                    usuarioTestDataBuilder.build();
+                },
+                ExcepcionValorObligatorio.class, "Se debe ingresar la clave");
+    }
+
+    @Test
+    void deberiaFallarSinTamanioClave() {
+
+        //Arrange
+        UsuarioTestDataBuilder usuarioTestDataBuilder = new UsuarioTestDataBuilder().conClave("123").conId(1L);
+        //act-assert
+        BasePrueba.assertThrows(() -> {
+                    usuarioTestDataBuilder.build();
+                },
+                ExcepcionLongitudValor.class, "La clave debe tener una longitud mayor o igual a 4");
+    }
+
+    @Test
+    void deberiaFallarSinFechaCreacion() {
+
+        //Arrange
+        UsuarioTestDataBuilder usuarioTestDataBuilder = new UsuarioTestDataBuilder().conFechaCreacion(null).conId(1L);
+        //act-assert
+        BasePrueba.assertThrows(() -> {
+                    usuarioTestDataBuilder.build();
+                },
+                ExcepcionValorObligatorio.class, "Se debe ingresar la fecha de creación");
+    }
+
+
+}
