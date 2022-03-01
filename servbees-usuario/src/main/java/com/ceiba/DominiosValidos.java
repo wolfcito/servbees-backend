a362dedad3a0ffa53829329e296a12a2b58344aa
Luis Fernando Ushiña <guffenix@gmail.com>
2022-03-01 16:20:31 -0500

    update valid domains

--- servbees-usuario/src/main/java/com/ceiba/DominiosValidos.java
+++ servbees-usuario/src/main/java/com/ceiba/DominiosValidos.java
@@ -1,2 +1,20 @@
-package com.ceiba;public class DominiosValidos {
+package com.ceiba;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class DominiosValidos {
+    @Bean
+    public WebMvcConfigurer configuradorCors() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+//                Colocar los dominios permitidos para consumo
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
 }
