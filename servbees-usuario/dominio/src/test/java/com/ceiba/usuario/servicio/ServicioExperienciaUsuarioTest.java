f79d397208eedc865c12f135f57c055bad596964
Luis Fernando Ushiña <guffenix@gmail.com>
2022-03-10 08:23:15 -0500

    complete usuario experiencia test

---
+++ servbees-usuario/dominio/src/test/java/com/ceiba/usuario/servicio/ServicioExperienciaUsuarioTest.java
@@ -0,0 +1,38 @@
+package com.ceiba.usuario.servicio;
+
+import com.ceiba.usuario.puerto.repositorio.RepositorioUsuario;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+final class ServicioExperienciaUsuarioTest {
+
+    @Test
+    @DisplayName("Deberia consultar si cumple la experiencia del usuario llamando al repositorio")
+    void deberiaConsultarSiCumpleExperienciaUsuarioLlamandoAlRepositorio() {
+        RepositorioUsuario repositorioUsuario = Mockito.mock(RepositorioUsuario.class);
+        Mockito.when(repositorioUsuario.experienciaPorIdUsuario(Mockito.anyLong())).thenReturn(true);
+        ServicioExperienciaUsuario servicioExperienciaUsuario = new ServicioExperienciaUsuario(repositorioUsuario);
+
+        boolean tieneExperiencia = servicioExperienciaUsuario.ejecutar(1L);
+        assertTrue(tieneExperiencia);
+        Mockito.verify(repositorioUsuario, Mockito.times(1)).experienciaPorIdUsuario(1L);
+
+    }
+
+    @Test
+    @DisplayName("Deberia lanzar una exepcion cuando se valide la inexistencia del Usuario")
+    void eberiaConsultarNoCumpleExperienciaUsuarioLlamandoAlRepositorio() {
+        RepositorioUsuario repositorioUsuario = Mockito.mock(RepositorioUsuario.class);
+        Mockito.when(repositorioUsuario.experienciaPorIdUsuario(Mockito.anyLong())).thenReturn(false);
+        ServicioExperienciaUsuario servicioExperienciaUsuario = new ServicioExperienciaUsuario(repositorioUsuario);
+
+        boolean tieneExperiencia = servicioExperienciaUsuario.ejecutar(10L);
+        assertFalse(tieneExperiencia);
+        Mockito.verify(repositorioUsuario, Mockito.times(1)).experienciaPorIdUsuario(10L);
+    }
+
+
+}
